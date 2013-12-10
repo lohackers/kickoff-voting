6fc23e9f99e65f6fe10b07deb3acbb298c00bdf7
Matteo Rosati <mrosati@h-art.it>
2013-12-10 11:57:22 +0100

    Add some seed data

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,38 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('ProjectTableSeeder');
 	}
 
+}
+
+class UserTableSeeder extends Seeder {
+	public function run()
+	{
+		User::create(array(
+			'email' => 'mrosati@h-art.com'
+		));
+		User::create(array(
+			'email' => 'gnegro@h-art.com'
+		));
+		User::create(array(
+			'email' => 'mfiaschini@h-art.com'
+		));
+	}
+}
+
+class ProjectTableSeeder extends Seeder {
+	public function run()
+	{
+		Project::create(array(
+			'name' => 'Progetto numero 1'
+		));
+		Project::create(array(
+			'name' => 'Progetto numero 2'
+		));
+		Project::create(array(
+			'name' => 'Progetto numero 3'
+		));
+	}
 }
