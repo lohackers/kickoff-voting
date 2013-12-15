3071bd057dca22e2c7a154e6909d9eb70236d333
Matteo Rosati <rosati.matteo@gmail.com>
2013-12-15 10:58:49 +0100

    Import Twitter Bootstrap

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,8 +3,11 @@
 <head>
 	<meta charset="utf-8">
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
 </head>
 <body>
-	@yield('content')
+	<div class="container">
+		@yield('content')
+	</div>
 </body>
 </html>
