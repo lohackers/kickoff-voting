f050f1e0267dd909ed2ab1675dc1bab697bcc8ac
Matteo Rosati <mrosati@h-art.it>
2013-12-19 10:54:25 +0100

    Update winner page style

--- app/views/projects/winner.blade.php
+++ app/views/projects/winner.blade.php
@@ -1,13 +1,10 @@
 @extends('layout')
 
 @section('content')
-	<div class="row col-xs-12">
-		<h1>And the winner is...</h1>
+	<div class="has-header has-subheader content padding scroll overflow-scroll">
 		@foreach($projects as $project)
-			<div class="jumbotron">
-				<h3>{{ $project->name }}</h3>
-				<h4>{{ count($project->votes) }} votes</h4>
-			</div>
+			<h4>{{ $project->name }}</h4>
+			<h5>{{ count($project->votes) }} votes</h5>
 		@endforeach
 	</div>
 @stop
